6d4b1df6725614b349c05182d6d6d9b303f3c078
Sam Rolfe <scrolfe@gmail.com>
2017-08-31 12:28:01 -0500

    more progress

---
+++ app/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import uuid from 'uuid';
+import Notes from '../Notes';
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do Laundry'
+  }
+]
+
+export default () => (
+  <div>
+    <button onClick={() => console.log('add note')}>+</button>
+    <Notes notes={notes}/>
+  </div>
+)
