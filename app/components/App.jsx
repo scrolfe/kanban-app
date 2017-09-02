054f46e64c7d4bcf53e6bf63d4d7b7f4c9285f5a
Sam Rolfe <scrolfe@gmail.com>
2017-09-02 15:09:13 -0500

    added lanes and their data dependencies

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,52 +1,28 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
+import Lanes from './Lanes';
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions';
+import LaneActions from '../actions/LaneActions';
 
 
-class App extends React.Component {
-
-  render() {
-    const { notes } = this.props;
-    return(
-      <div>
-
-        <button className='add-note' onClick={this.addNote}>+</button>
-        <Notes
-          notes={notes}
-          onNoteClick={this.activateNoteEdit}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote}
-        />
-      </div>
-    )
-  }
-
-  addNote = () => {
-    this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
       id: uuid.v4(),
-      task: 'New Task'
+      name: 'New Lane'
     });
-  }
+  };
 
-  activateNoteEdit = (id) => {
-    this.props.NoteActions.update({id, editing: true});
-  }
+  return(
+    <div>
+      <button className="add-lane" onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  )
+};
 
-  editNote = (id, task) => {
-    this.props.NoteActions.update({id, task, editing: false});
-  }
-
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-
-    this.props.NoteActions.delete(id);
-  }
-}
-
-export default connect(({notes}) => ({
-  notes
+export default connect(({lanes}) => ({
+  lanes
 }), {
-  NoteActions
+  LaneActions
 })(App)
--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,5 +1,57 @@
 import React from 'react';
+import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
+import Notes from './Notes';
+import LaneActions from '../actions/LaneActions';
+import LaneHeader from './LaneHeader';
 
-export default ({lane, ...props}) => (
-  <div {...props}>{lane.name}</div>
-)
+const Lane = ({
+  lane, notes, LaneActions, NoteActions, ...props
+}) => {
+  const editNote = (id, task) => {
+    NoteActions.update({id, task, editing: false});
+  }
+
+  const deleteNote = (noteId, e) => {
+    e.stopPropagation();
+
+    LaneActions.detachFromLane({
+      laneId: lane.id,
+      noteId
+    });
+
+    NoteActions.delete(noteId)
+  };
+
+  const activateNoteEdit = id => {
+    NoteActions.update({ id, editing: true});
+  };
+
+  return (
+    <div {...props}>
+      <LaneHeader lane={lane} />
+      <Notes
+        notes={selectNotesByIds(notes, lane.notes)}
+        onNoteClick={activateNoteEdit}
+        onEdit={editNote}
+        onDelete={deleteNote} />
+    </div>
+  );
+};
+
+function selectNotesByIds(allNotes, noteIds = []) {
+  return noteIds.reduce((notes, id) =>
+    notes.concat(
+      allNotes.filter(note => note.id === id)
+    )
+  , []);
+}
+
+export default connect(
+  ({notes}) => ({
+    notes
+  }), {
+    NoteActions,
+    LaneActions
+  }
+)(Lane)
