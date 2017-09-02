f5fae8d2a01a66755088bca27e81290f14687261
Sam Rolfe <scrolfe@gmail.com>
2017-09-02 13:33:37 -0500

    implemented localStorage keeper

--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,5 +1,9 @@
 import NoteStore from '../../stores/NoteStore';
+import storage from '../../libs/storage';
+import persist from '../../libs/persist';
 
 export default alt => {
   alt.addStore('NoteStore', NoteStore);
+
+  persist(alt, storage(localStorage), 'app');
 }
