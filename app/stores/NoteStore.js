563c7ad61d22d6b422f749795b36989808ecc66e
Sam Rolfe <scrolfe@gmail.com>
2017-09-02 12:56:50 -0500

    full implementation of flux architecture

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,7 +1,11 @@
 import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
 
 export default class NoteStore {
   constructor() {
+
+    this.bindActions(NoteActions);
+
     this.notes = [
       {
         id: uuid.v4(),
@@ -12,5 +16,26 @@ export default class NoteStore {
         task: 'Do Laundry'
       }
     ];
+  }
+
+  create(note) {
+    this.setState({
+      notes: this.notes.concat(note)
+    });
+  }
+  update(updatedNote) {
+    this.setState({
+      notes: this.notes.map(note => {
+        if(note.id === updatedNote.id) {
+          return Object.assign({}, note, updatedNote);
+        }
+        return note;
+      })
+    });
+  }
+  delete(id) {
+    this.setState({
+      notes: this.notes.filter(note => note.id !== id)
+    });
   }
 }
